72df062c2fc893afa78980c18a4c9dbb49ed0f65
Mayank Jain <jainmayank2509@gmail.com>
2023-07-01 13:30:12 +0530

    added remaining features

--- src/pages/adminDashboard.js
+++ src/pages/adminDashboard.js
@@ -1,28 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import Router from 'next/router'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import IssueDisplay from '../components/IssueDisplay'
+import styles from '../components/FrontComponent.module.css'
+import axios from 'axios'
 
 const adminDashboard = () => {
 
     const {status,data} = useSession()
+    const [stat, setStat] = useState('unauthenticated')
+    const [adminInfo, setAdminInfo] = useState({})
 
+    const getAdminData = async () => {
+      const adminData = await axios.get(`http://localhost:5001/admin/${data.user.email}`,{
+        headers:{
+          'Content-Type':'application/json',
+        }
+      })
+      setAdminInfo(adminData.data)
+    }
     useEffect(()=>{
         if(status==="unauthenticated"){
           // alert("You are not logged in! Redirecting to login page...")
           Router.push('/login')
         }
-        console.log(status)
+        else if(status==="authenticated"){
+          getAdminData()
+        }
      },[status])
-  return (
-    <div>
-        <Head>
-            <title>Admin Dashboard - HMS</title>
-        </Head>
-        <Navbar logOut={true} admin={true} />
-    </div>
-  )
+
+     
+
+     if(status=='authenticated'){
+      return (
+        data && <div>
+            <Head>
+                <title>Admin Dashboard - HMS</title>
+            </Head>
+            <Navbar logOut={true} admin={true} />
+            <center>  
+              <h2 className={styles.mainText}><i>hola,</i>{data.user.name} </h2>
+              <p style={{marginTop:'-10px'}}>How's your day today?</p>
+              
+              <IssueDisplay email={adminInfo.email} hostelName={adminInfo.hostelName}/>
+            
+    
+            </center>   
+        </div>
+      )
+     }
+  
 }
 
 export default adminDashboard
